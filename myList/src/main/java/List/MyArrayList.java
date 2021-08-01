280980ffc490904be9fa029a3097da8bac34da7d
Valinstein <valinstein@ukr.net>
2021-08-01 17:06:03 +0300

    MyList was added

---
+++ myList/src/main/java/List/MyArrayList.java
@@ -0,0 +1,177 @@
+package List;
+
+import java.util.ArrayList;
+
+public class MyArrayList implements List {
+
+    Object[] objects;
+    int size;
+
+    public MyArrayList() {
+        this.objects = new Object[10];
+        this.size = 0;
+    }
+
+
+    @Override
+    public void add(Object value) {
+        arrayExtension();
+        this.objects[size] = value;
+        this.size++;
+    }
+
+    @Override
+    public void add(Object value, int index) {
+        indexOutOfBoundsExceptionCheck(index);
+        arrayExtension();
+        if (size != 0) {
+            this.add(this.objects[size - 1]);
+            for (int i = size - 2; i > index; i--) {
+                this.objects[i] = this.objects[i - 1];
+            }
+            this.objects[index] = value;
+        } else {
+            this.add(value);
+        }
+    }
+
+    @Override
+    public Object remove(int index) {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        indexOutOfBoundsExceptionCheck(index);
+        Object removedObject = this.objects[index];
+        for (int i = index; i < this.size - 1; i++) {
+            this.objects[i] = this.objects[i + 1];
+        }
+        this.size--;
+        return removedObject;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        indexOutOfBoundsExceptionCheck(index);
+        return this.objects[index];
+    }
+
+    @Override
+    public Object set(Object value, int index) {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        indexOutOfBoundsExceptionCheck(index);
+        Object oldObject = this.objects[index];
+        this.objects[index] = value;
+        return oldObject;
+    }
+
+    @Override
+    public void clear() {
+        this.objects = new Object[10];
+        this.size=0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (this.size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        if (indexOf(value) == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int indexOf(Object value) {
+        int noValueIndex = -1;
+        for (int i = 0; i < this.size; i++) {
+            if(value==null){
+                if (this.objects[i]==null) {
+                    return i;
+                }
+            }
+            if (this.objects[i].equals(value)) {
+                return i;
+            }
+        }
+        return noValueIndex;
+    }
+
+    @Override
+    public int lastIndexOf(Object value) {
+        int noValueIndex = -1;
+        for (int i = size - 1; i >= 0; i--) {
+            if(value==null){
+                if (this.objects[i]==null) {
+                    return i;
+                }
+            }
+            if (this.objects[i].equals(value)) {
+                return i;
+            }
+        }
+        return noValueIndex;
+    }
+
+    private void indexOutOfBoundsExceptionCheck(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+
+    private int indexOfLastElement() {
+        int lastElement = 0;
+        for (int i = this.objects.length - 1; i > 0; i--) {
+            if (!this.objects[i].equals(null)) {
+                lastElement = i;
+            }
+        }
+        return lastElement;
+    }
+
+    private void arrayExtension() {
+        if (size == this.objects.length) {
+            int newSize = (int) (this.objects.length * 1.5 + 1);
+            Object[] newArray = new Object[newSize];
+            for (int i = 0; i <= this.size; i++) {
+                newArray[i] = this.objects[i];
+            }
+            this.objects = newArray;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i <size ; i++) {
+            if(this.objects[i] == null){
+                stringBuilder.append("null");
+            }
+            if(this.objects[i] != null)  {
+                stringBuilder.append(this.objects[i].toString());
+            }
+            if(i != size-1){
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ myList/src/test/java/List/MyArrayListTest.java
@@ -0,0 +1,475 @@
+package List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MyArrayListTest {
+    private List list = new MyArrayList();
+
+    @Test
+    void addWithIndexFirstLastAndMiddleElementTest() {
+        list.add("First", 0);
+        list.add("Next", 1);
+        list.add("Last", 2);
+        list.add("Middle", 1);
+        assertEquals(4, list.size());
+        assertEquals("First", list.get(0));
+        assertEquals("Middle", list.get(1));
+        assertEquals("Next", list.get(2));
+        assertEquals("Last", list.get(3));
+    }
+
+    @Test
+    void addWithoutIndexTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertEquals(3, list.size());
+        assertEquals("green", list.get(0));
+        assertEquals("white", list.get(1));
+        assertEquals("yellow", list.get(2));
+    }
+
+    @Test
+    void addFirstElementByIndexInClearedListTest() {
+
+        list.add("1", 0);
+        assertEquals(1, list.size());
+        assertEquals("1", list.get(0));
+    }
+
+    @Test
+    void addNullElementWithIndexTest() {
+        list.add("one", 0);
+        list.add(null, 0);
+        list.add(null, 2);
+        list.add(null, 1);
+
+        assertEquals(4, list.size());
+
+        assertNull(list.get(0));
+        assertNull(list.get(1));
+        assertEquals("one", list.get(2));
+        assertNull(list.get(3));
+    }
+
+    @Test
+    void addNullElementWithoutIndexTest() {
+        list.add(null);
+        list.add("one");
+        list.add(null);
+
+        assertEquals(3, list.size());
+
+        assertNull(list.get(0));
+        assertEquals("one", list.get(1));
+        assertNull(list.get(2));
+    }
+
+    @Test
+    void addElementByIndexOutOfBoundsTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.add("-10", -10);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.add("20", 20);
+        });
+    }
+
+    @Test
+    void removeFirstLastAndMiddleByIndexTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        list.add("white");
+        list.add("black");
+        assertEquals(5, list.size());
+
+        assertEquals("green", list.remove(0));
+        assertEquals("black", list.remove(3));
+        assertEquals("yellow", list.remove(1));
+
+        assertEquals(2, list.size());
+
+        assertEquals("white", list.remove(0));
+        assertEquals("white", list.remove(0));
+
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    void removeNullElementByIndexTest() {
+        list.add("1");
+        list.add(null);
+        list.add("2");
+        assertNull(list.remove(1));
+        assertEquals(2, list.size());
+    }
+
+    //    @Test
+//    void removeFirstLastAndMiddleElementByElementTest() {
+//        list.add("green");
+//        list.add("white");
+//        list.add("yellow");
+//        list.add("white");
+//        list.add("black");
+//        assertEquals(5, list.size());
+//
+//        assertTrue(list.remove("green"));
+//        assertTrue(list.remove("black"));
+//        assertTrue(list.remove("yellow"));
+//
+//        assertEquals(2, list.size());
+//
+//        assertEquals("white", list.get(0));
+//        assertEquals("white", list.get(1));
+//    }
+//
+//    @Test
+//    void removeNonexistentElementByElementTest() {
+//        list.add("green");
+//        list.add("white");
+//        list.add("yellow");
+//        assertEquals(3, list.size());
+//
+//        assertFalse(list.remove("red"));
+//    }
+//
+//    @Test
+//    void removeNullElementTest() {
+//        list.add(null);
+//        list.add("hello");
+//        assertEquals(2, list.size());
+//        assertTrue(list.remove(null));
+//        assertEquals(1, list.size());
+//    }
+    @Test
+    void removeElementByIndexOutOfBoundsTest() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(0);
+        });
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(5);
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(13);
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.remove(-10);
+        });
+    }
+
+    @Test
+    void getElementByIndexTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertEquals("green", list.get(0));
+        assertEquals("white", list.get(1));
+        assertEquals("yellow", list.get(2));
+    }
+
+    @Test
+    void getNullElementTest() {
+        list.add(null);
+        list.add("second");
+        assertNull(list.get(0));
+    }
+    @Test
+    void getElementByIndexOutOfBoundsTest() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.get(0);
+        });
+
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.get(10);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.get(-10);
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.get(5);
+        });
+    }
+
+    @Test
+    void setFirstLastAndMiddleElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        assertEquals("green", list.set("red", 0));
+        assertEquals("white", list.set("brown", 1));
+        assertEquals("yellow", list.set("pink", 2));
+
+        assertEquals("red", list.get(0));
+        assertEquals("brown", list.get(1));
+        assertEquals("pink", list.get(2));
+
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    void setElementInListWithOneElementTest() {
+        list.add("first");
+        assertEquals("first", list.set("replacement", 0));
+        assertEquals("replacement", list.get(0));
+    }
+
+    @Test
+    void setNullElementTest() {
+        list.add("First");
+        list.add(null);
+        list.add("Last");
+
+        assertNull(list.set("value", 1));
+        assertEquals("value", list.get(1));
+    }
+
+    @Test
+    void setElementByIndexOutOfBoundsTest() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.set("red", 0);
+        });
+
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.set("-2", -2);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.set("100", 100);
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.set("5", 5);
+        });
+    }
+
+    @Test
+    void indexOfWithExistElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        list.add("white");
+        list.add("black");
+
+        assertEquals(0, list.indexOf("green"));
+        assertEquals(1, list.indexOf("white"));
+        assertEquals(2, list.indexOf("yellow"));
+        assertEquals(4, list.indexOf("black"));
+
+    }
+
+    @Test
+    void indexOfWithNonexistentElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertEquals(-1, list.indexOf("red"));
+        assertEquals(-1, list.indexOf("blue"));
+
+    }
+
+    @Test
+    void lastIndexOfWithExistElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+        list.add("white");
+        list.add("black");
+
+        assertEquals(0, list.lastIndexOf("green"));
+        assertEquals(3, list.lastIndexOf("white"));
+        assertEquals(2, list.lastIndexOf("yellow"));
+        assertEquals(4, list.lastIndexOf("black"));
+    }
+    @Test
+    void lastIndexOfWithNonexistentElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertEquals(-1, list.lastIndexOf("brown"));
+        assertEquals(-1, list.lastIndexOf("red"));
+    }
+
+    @Test
+    void indexOfWithNullElementTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        list.add(null);
+        list.add("black");
+
+        assertEquals(1, list.indexOf(null));
+
+    }
+
+    @Test
+    void lastIndexOfWithNullElementTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        list.add(null);
+        list.add("black");
+
+        assertEquals(3, list.lastIndexOf(null));
+    }
+
+    @Test
+    void containsNotNullElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertTrue(list.contains("green"));
+        assertTrue(list.contains("white"));
+        assertTrue(list.contains("yellow"));
+
+    }
+
+    @Test
+    void containsWithMissingElementElementTest() {
+        list.add("green");
+        list.add("white");
+        list.add("yellow");
+
+        assertFalse(list.contains("pink"));
+        assertFalse(list.contains("red"));
+    }
+
+    @Test
+    void containsNullElementTest() {
+        list.add(null);
+        list.add("element");
+        list.add(null);
+        list.add("last");
+        assertTrue(list.contains(null));
+    }
+
+    @Test
+    void clearTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        assertEquals(3, list.size());
+
+        list.clear();
+
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    void isEmptyTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        assertFalse(list.isEmpty());
+        list.clear();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    void sizeTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        assertEquals(3, list.size());
+        list.add("brown");
+        assertEquals(4, list.size());
+        list.clear();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    void toStringTest() {
+        list.add("green");
+        list.add(null);
+        list.add("yellow");
+        assertEquals("[green, null, yellow]", list.toString());
+        list.remove(0);
+        assertEquals("[null, yellow]", list.toString());
+        list.add("red");
+        assertEquals("[null, yellow, red]", list.toString());
+        list.clear();
+        assertEquals("[]", list.toString());
+        list.add("first");
+        assertEquals("[first]", list.toString());
+    }
+
+
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
